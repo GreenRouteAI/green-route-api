1d3831a45014fc4ca3dc47a85ecc164009f1566c
NyH1409 <hei.nyhasina@gmail.com>
2024-04-02 09:08:15 +0300

    chore: gemini model conf

---
+++ src/main/java/app/green/route/service/ai/GeminiService.java
@@ -0,0 +1,9 @@
+package app.green.route.service.ai;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GeminiService {
+}
