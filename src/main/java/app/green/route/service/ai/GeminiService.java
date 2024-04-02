08195597d01f1a67662c385390d7b9d417ef6d2a
NyH1409 <hei.nyhasina@gmail.com>
2024-04-02 10:56:07 +0300

    chore: gemini service

--- src/main/java/app/green/route/service/ai/GeminiService.java
+++ src/main/java/app/green/route/service/ai/GeminiService.java
@@ -1,9 +1,21 @@
 package app.green.route.service.ai;
 
+import app.green.route.service.ai.conf.GeminiConf;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class GeminiService {
+  private final GeminiConf conf;
+
+  public GenerateContentResponse generateContent(String prompt){
+    try {
+      return conf.getModel().generateContent(prompt);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
