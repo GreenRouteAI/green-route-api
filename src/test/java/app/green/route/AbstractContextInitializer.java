d94cdf6ad4a27420c2fe975f6b3c579de48e2825
NyH1409 <hei.nyhasina@gmail.com>
2024-04-02 13:21:05 +0300

    chore: ping IT

--- src/test/java/app/green/route/AbstractContextInitializer.java
+++ src/test/java/app/green/route/AbstractContextInitializer.java
@@ -7,24 +7,26 @@ import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-public class AbstractContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+public abstract class AbstractContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
   private static final String FLYWAY_TESTDATA_PATH = "classpath:/db/testdata";
-
-  private PostgreSQLContainer<?> postgresContainer() {
-    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
-        .withDatabaseName("dummy")
-        .withUsername("dummy")
-        .withPassword("dummy");
-  }
 
   @Override
   public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+    var postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+        .withDatabaseName("dummy")
+        .withUsername("dummy")
+        .withPassword("dummy");
+
+    postgresContainer.start();
+
     TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
         applicationContext,
-        "server.port=8080",
-        "spring.datasource.url=" + postgresContainer().getJdbcUrl(),
-        "spring.datasource.username=" + postgresContainer().getUsername(),
-        "spring.datasource.password=" + postgresContainer().getPassword(),
+        "server.port=" + this.getServerPort(),
+        "spring.datasource.url=" + postgresContainer.getJdbcUrl(),
+        "spring.datasource.username=" + postgresContainer.getUsername(),
+        "spring.datasource.password=" + postgresContainer.getPassword(),
         "spring.flyway.locations=classpath:/db/migration," + FLYWAY_TESTDATA_PATH);
   }
+
+  public abstract int getServerPort();
 }
