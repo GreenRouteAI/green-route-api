1659c00437b7608d3032ef0cecee0ec9b4ba4682
NyH1409 <hei.nyhasina@gmail.com>
2024-04-04 19:15:58 +0300

    chore: sentry conf

---
+++ src/main/java/app/green/route/SentryConf.java
@@ -0,0 +1,33 @@
+package app.green.route;
+
+import io.sentry.Sentry;
+import io.sentry.SentryOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SentryConf {
+  private final String sentryDsn;
+  private final String env;
+
+  private static final double TRACES_SAMPLE_RATE = 1.0;
+
+  public SentryConf(
+      @Value("${sentry.dsn}") String sentryDsn, @Value("${sentry.environment}") String env) {
+    this.sentryDsn = sentryDsn;
+    this.env = env;
+  }
+
+  @Bean
+  public Sentry.OptionsConfiguration<SentryOptions> getSentryOptionsConf() {
+    Sentry.OptionsConfiguration<SentryOptions> optionsConf =
+        options -> {
+          options.setDsn(sentryDsn);
+          options.setEnvironment(env);
+          options.setTracesSampleRate(TRACES_SAMPLE_RATE);
+        };
+    Sentry.init(optionsConf);
+    return optionsConf;
+  }
+}
