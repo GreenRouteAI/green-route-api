27fe1f2a36346c87f25064b65506a2a3d7c7092b
NyH1409 <hei.nyhasina@gmail.com>
2024-04-08 16:16:09 +0300

    feat: authentication resource

---
+++ src/main/java/app/green/route/endpoint/controller/SecurityController.java
@@ -0,0 +1,30 @@
+package app.green.route.endpoint.controller;
+
+import app.green.route.endpoint.security.AuthProvider;
+import app.green.route.endpoint.mapper.UserRestMapper;
+import app.green.route.endpoint.rest.model.User;
+import app.green.route.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class SecurityController {
+  private final UserService service;
+  private final UserRestMapper mapper;
+
+  @PostMapping("/signin")
+  public User signIn() {
+    var authenticatedUser = AuthProvider.getUser();
+    return mapper.toRest(authenticatedUser);
+  }
+
+  @PostMapping("/signup")
+  public User signUp(@RequestBody User payload) {
+    var toSave = mapper.toDomain(payload);
+    var saved = service.save(toSave);
+    return mapper.toRest(saved);
+  }
+}
