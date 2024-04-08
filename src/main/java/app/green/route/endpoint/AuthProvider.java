fcd75649d02203491aaf5053c231b04316f69b2b
NyH1409 <hei.nyhasina@gmail.com>
2024-04-08 12:40:46 +0300

    feat: oauth security conf

---
+++ src/main/java/app/green/route/endpoint/AuthProvider.java
@@ -0,0 +1,49 @@
+package app.green.route.endpoint;
+
+import app.green.route.Principal;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthProvider extends AbstractUserDetailsAuthenticationProvider {
+  private static final String BEARER_PREFIX = "Bearer ";
+
+  private static Principal getPrincipal() {
+    SecurityContext context = SecurityContextHolder.getContext();
+    Object principal = context.getAuthentication().getPrincipal();
+    return ((Principal) principal);
+  }
+
+  public static String getBearer(HttpServletRequest req) {
+    String authorization = req.getHeader("Authorization");
+    if (authorization != null && authorization.startsWith(BEARER_PREFIX)) {
+      return authorization.substring(BEARER_PREFIX.length());
+    }
+    return null;
+  }
+
+  public static User getUser() {
+    return getPrincipal().getUser();
+  }
+
+  public static String getBearer() {
+    return getPrincipal().getBearer();
+  }
+
+  @Override
+  protected void additionalAuthenticationChecks(
+      UserDetails userDetails, UsernamePasswordAuthenticationToken authentication)
+      throws AuthenticationException {}
+
+  @Override
+  protected UserDetails retrieveUser(
+      String username, UsernamePasswordAuthenticationToken authentication)
+      throws AuthenticationException {
+    return (Principal) authentication.getPrincipal();
+  }
+}
