281a823f497f63166abd0e3f5134c2e4a665d686
NyH1409 <hei.nyhasina@gmail.com>
2024-04-10 18:51:42 +0300

    chore: data validator

--- src/main/java/app/green/route/endpoint/controller/RouteController.java
+++ src/main/java/app/green/route/endpoint/controller/RouteController.java
@@ -3,6 +3,7 @@ package app.green.route.endpoint.controller;
 import app.green.route.endpoint.mapper.RouteRestMapper;
 import app.green.route.endpoint.rest.model.Itinerary;
 import app.green.route.endpoint.rest.model.TravelDescription;
+import app.green.route.endpoint.validator.TravelValidator;
 import app.green.route.service.RouteService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class RouteController {
   private final RouteService service;
   private final RouteRestMapper mapper;
+  private final TravelValidator validator;
 
   @PostMapping(value = "/itineraries")
   public Itinerary generateItineraries(@RequestBody TravelDescription travelDescription) {
+    validator.accept(travelDescription);
     var transport = mapper.toDomainTravelPayload(travelDescription);
     var accommodation = mapper.toDomainAccommodationPayload(travelDescription);
     return service.generateItineraries(transport, accommodation, travelDescription);
---
+++ src/main/java/app/green/route/endpoint/validator/TravelValidator.java
@@ -0,0 +1,33 @@
+package app.green.route.endpoint.validator;
+
+import app.green.route.endpoint.rest.model.TravelDescription;
+import app.green.route.model.exception.BadRequestException;
+import java.util.function.Consumer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TravelValidator implements Consumer<TravelDescription> {
+  @Override
+  public void accept(TravelDescription user) {
+    var builder = new StringBuilder();
+    if (user.getPeople() == null) {
+      builder.append("People's number is mandatory. ");
+    }
+    if (user.getAccommodationType() == null) {
+      builder.append("AccommodationType is mandatory. ");
+    }
+    if (user.getVehicle().getType() == null) {
+      builder.append("VehicleType is mandatory");
+    }
+    if (user.getVehicle().getFuel().getType() == null) {
+      builder.append("FuelType is mandatory. ");
+    }
+    if (user.getDistance() == null) {
+      builder.append("Distance is mandatory. ");
+    }
+
+    if (!builder.isEmpty()) {
+      throw new BadRequestException(builder.toString());
+    }
+  }
+}
